1e8aafbf6437ec4a5a4bcb4db17d5f0cf67bff45
Binod Chaudhary <thebinod7@gmail.com>
2024-11-15 09:22:02 +0545

    deploy and copy ABIs

---
+++ scripts/deploy.js
@@ -0,0 +1,57 @@
+const { network } = require("hardhat");
+const path = require("path");
+
+const CONTRACT_NAME = "Token";
+
+async function main() {
+  if (network.name === "localhost") {
+    console.warn(
+      "You are trying to deploy a contract to the Hardhat Network, which" +
+        "gets automatically created and destroyed every time. Use the Hardhat" +
+        " option '--network localhost'"
+    );
+  }
+
+  // ethers is available in the global scope
+  const [deployer] = await ethers.getSigners();
+  console.log("Deployer address:", deployer.address);
+
+  const Token = await ethers.getContractFactory(CONTRACT_NAME);
+  const token = await Token.deploy();
+
+  saveFrontendFiles(token);
+}
+
+function saveFrontendFiles(token) {
+  const fs = require("fs");
+  const contractsDir = path.join(
+    __dirname,
+    "..",
+    "frontend",
+    "app",
+    "contracts"
+  );
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    path.join(contractsDir, "contract-address.json"),
+    JSON.stringify({ Token: token.runner.address }, undefined, 2)
+  );
+
+  const TokenArtifact = artifacts.readArtifactSync("Token");
+
+  fs.writeFileSync(
+    path.join(contractsDir, "Token.json"),
+    JSON.stringify(TokenArtifact, null, 2)
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
